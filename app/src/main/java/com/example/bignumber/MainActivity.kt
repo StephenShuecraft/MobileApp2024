39f6f8101d6cc45b67be5e2d337005b917011a10
StephenShuecraft <stephenshuecraft@gmail.com>
2024-02-12 09:41:47 -0600

    Added Image with win state

--- app/src/main/java/com/example/bignumber/MainActivity.kt
+++ app/src/main/java/com/example/bignumber/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
-    private var leftNum :Int = 0;
-    private var rightNum :Int = 0;
-    private var score :Int = 0;
+    private var leftNum :Int = 0
+    private var rightNum :Int = 0
+    private var score :Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,31 +19,45 @@ class MainActivity : AppCompatActivity() {
          // above init our app ui
 
 
-        pickRandomNumbers();
+        pickRandomNumbers()
     }
 
     fun leftButtonOnClick(view: View)
     {
-        pickRandomNumbers();
+        if(leftNum > rightNum)
+            score++
+        pickRandomNumbers()
     }
 
     fun rightButtonOnClick(view: View)
     {
-        pickRandomNumbers();
+        if(leftNum < rightNum)
+            score++
+        pickRandomNumbers()
     }
 
     fun pickRandomNumbers()
     {
-        var leftButton = findViewById<Button>(R.id.left_number_button);
-        var rightButton = findViewById<Button>(R.id.right_number_button);
+        var leftButton = findViewById<Button>(R.id.left_number_button)
+        var rightButton = findViewById<Button>(R.id.right_number_button)
 
-        var rand = Random();
+        var rand = Random()
 
-        leftNum = rand.nextInt(10);
-        rightNum = rand.nextInt(10);
+        leftNum = rand.nextInt(10)
+        rightNum = rand.nextInt(10)
 
-        leftButton.text = "$leftNum";
-        rightButton.text = "$rightNum";
+        leftButton.text = "$leftNum"
+        rightButton.text = "$rightNum"
     }
 
+    fun updateScoreText()
+    {
+        var scoreText = findViewById<TextView>(R.id.score_text)
+        var winImage = findViewById<ImageView>(R.id.win_image)
+        if (score >= 5)
+            winImage.visibility = View.VISIBLE
+        else
+            winImage.visibility = View.INVISIBLE
+        scoreText.text = "Score: $score"
+    }
 }
